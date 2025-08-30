972cf098d48887b9c94126cbb37f8320f68d202e
Amarjit-paswan <Amarjit-paswan>
2025-08-30 21:46:28 +0530

    color code add

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<style>
+    :root{
+        --background_color : #0D1B2A;
+        --text_color: #F5F7FA;
+        --button_color:#00F5D4;
+        --button_color2: #FF5C8A;
+    }
+
+    .bg_color{
+        background-color: var(--background_color);
+        color:var(--text_color);
+    }
+
+    .text_color{
+        color: var(--text_color)
+    }
+
+    .button_color_1{
+        background-color: var(--button_color);
+    }
+    .button_color_2{
+        background-color: var(--button_color2);
+    }
+    body{
+        background-color: #0D1B2A;
+    }
+</style>
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            
+        </div>
+    </div>
+</body>
+</html>
